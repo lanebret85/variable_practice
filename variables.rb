0cc838095fffcbac109e9bcbe6720e2256ffb00c
Lane Bretschneider <lanebret85@gmail.com>
2023-06-16 13:59:45 -0700

    reassign string variables and use puts command for all variables

--- variables.rb
+++ variables.rb
@@ -13,3 +13,23 @@ kg = 2.2
 is_hungry = true
 is_sleepy = false
 is_happy = true
+
+day = "Thursday"
+location = "Denver"
+program = "General Assembly"
+
+puts day
+puts location
+puts program
+
+puts month
+puts day_of_month
+puts year
+
+puts pi
+puts km
+puts kg
+
+puts is_hungry
+puts is_sleepy
+puts is_happy
